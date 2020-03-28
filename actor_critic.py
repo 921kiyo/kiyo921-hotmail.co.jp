c67069199208a4498102509208d619afd2a65b41
Kiyo <921kiyo@users.noreply.github.com>
2020-03-28 12:13:12 +0000

    add monitor

--- actor_critic.py
+++ actor_critic.py
@@ -1,5 +1,5 @@
 import gym
-
+from gym import wrappers
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -13,10 +13,12 @@ from torch.distributions import Categorical
 from collections import deque
 
 env = gym.make("CartPole-v0")
+env = wrappers.Monitor(env, "reinforce", force=True)
 env.seed(0)
 
+
 class Policy(nn.Module):
-    def __init__(self, s, h, a):
+    def __init__(self, s=4, h=16, a=2):
         super().__init__()
         self.fc1 = nn.Linear(s, h)
         self.fc2 = nn.Linear(h, a)
@@ -28,18 +30,20 @@ class Policy(nn.Module):
         return F.softmax(state, dim=1)
 
     def choose_action(self, state):
-        state = torch.from_numpy(state)
+        state = torch.from_numpy(state).float().unsqueeze(0)
         probs = self.forward(state)
         m = Categorical(probs)
         action = m.sample()
 
         return action.item(), m.log_prob(action)
 
+
 policy = Policy()
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 
+
 def reinforce(n_episodes=1000, max_time=1000, gamma=1.0, print_every=100):
-    for episode in range(1, n_episodes+1):
+    for episode in range(1, n_episodes + 1):
         scores_deque = deque(maxlen=100)
         log_probs_tensor = []
         rewards = []
@@ -57,8 +61,8 @@ def reinforce(n_episodes=1000, max_time=1000, gamma=1.0, print_every=100):
         scores_deque.append(sum(rewards))
         scores.append(sum(rewards))
 
-        discounts= [gamma**i for i in range(len(rewards) + 1)]
-        R = sum([d*r for d, r in zip(discounts, rewards)])
+        discounts = [gamma ** i for i in range(len(rewards) + 1)]
+        R = sum([d * r for d, r in zip(discounts, rewards)])
 
         policy_loss = []
         for log_prob in log_probs_tensor:
@@ -66,20 +70,31 @@ def reinforce(n_episodes=1000, max_time=1000, gamma=1.0, print_every=100):
             # ??
         policy_loss = torch.cat(policy_loss).sum()
 
-
         optimizer.zero_grad()
         policy_loss.backward()
         optimizer.step()
 
         if episode % print_every == 0:
-            print('Episode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_deque)))
-        if np.mean(scores_deque)>=195.0:
-            print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_deque)))
+            print(
+                "Episode {}\tAverage Score: {:.2f}".format(
+                    episode, np.mean(scores_deque)
+                )
+            )
+        if np.mean(scores_deque) >= 195.0:
+            print(
+                "Environment solved in {:d} episodes!\tAverage Score: {:.2f}".format(
+                    episode - 100, np.mean(scores_deque)
+                )
+            )
             break
     return scores
 
-answer = reinforce()
 
+result = reinforce()
 
-
-
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(result) + 1), result)
+plt.ylabel("Score")
+plt.xlabel("Episode #")
+plt.show()
